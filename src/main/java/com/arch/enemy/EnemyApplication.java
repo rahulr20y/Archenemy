c4c5f36c95bf769b68dc5e8c07a9399078ed8cfa
rahulr20y <rahulr20y@gmail.com>
2023-12-06 19:35:13 +0530

    project initialization

---
+++ src/main/java/com/arch/enemy/EnemyApplication.java
@@ -0,0 +1,13 @@
+package com.arch.enemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arch/enemy/EnemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch.enemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
